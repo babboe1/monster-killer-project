1b4c032d33a2a9a23e493d312fcd438879d28c9e
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2020-12-07 17:01:05 +0100

    added consts

---
+++ assets/Scripts/vendor.js
@@ -0,0 +1,8 @@
+const monsterHealthBar = document.getElementById('monster-health');
+const playerHealthBar = document.getElementById('player-health');
+const bonusLifeEl = document.getElementById('bonus-life');
+
+const attackBtn = document.getElementById('attack-btn');
+const strongAttackBtn = document.getElementById('strong-attack-btn');
+const healBtn = document.getElementById('heal-btn');
+const logBtn = document.getElementById('log-btn');
