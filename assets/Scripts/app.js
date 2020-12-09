416df74e66005ec6a9294bc826be39ae578343e1
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2020-12-09 14:09:19 +0100

    fuction and event listener

--- assets/Scripts/app.js
+++ assets/Scripts/app.js
@@ -3,3 +3,10 @@ const ATTACK_VALUE = 10;
 let chosenMaxLIfe = 100;
 let currentMonsterHealth = chosenMaxLIfe;
 
+adjustHealthBars(chosenMaxLIfe);
+
+function attackHandler() {
+   const damage = dealMonsterDamage(ATTACK_VALUE);
+}
+
+attackBtn.addEventListener('click',attackHandler);
