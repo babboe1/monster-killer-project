a6ecd5006eed822c90d4d6f2fdee2e96c575432c
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2020-12-09 15:25:38 +0100

    if statements

--- assets/Scripts/app.js
+++ assets/Scripts/app.js
@@ -12,10 +12,12 @@ function attackHandler() {
    currentMonsterHealth -= damage;
    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
    currentPlayerHealth -= playerDamage;
-   if (currentMonsterHealth <= 0) {
+   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
       alert('you won! ');
-   } else if (currentPlayerHealth <= 0) {
+   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
       alert('you lost!');
+   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+      alert('you have a draw!!')
    }
 }
 
