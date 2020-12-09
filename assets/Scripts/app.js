caeab19ce80fd54828cd402fd24182c33c3abe0c
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2020-12-09 15:16:07 +0100

    added if statement

--- assets/Scripts/app.js
+++ assets/Scripts/app.js
@@ -1,14 +1,22 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLIfe = 100;
 let currentMonsterHealth = chosenMaxLIfe;
-let currentPLayerHealth = chosenMaxLIfe;
+let currentPlayerHealth = chosenMaxLIfe;
 
 adjustHealthBars(chosenMaxLIfe);
 
 function attackHandler() {
    const damage = dealMonsterDamage(ATTACK_VALUE);
    currentMonsterHealth -= damage;
+   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+   currentPlayerHealth -= playerDamage;
+   if (currentMonsterHealth <= 0) {
+      alert('you won! ');
+   } else if (currentPlayerHealth <= 0) {
+      alert('you lost!');
+   }
 }
 
-attackBtn.addEventListener('click',attackHandler);
+attackBtn.addEventListener('click', attackHandler);
