f19ce37624e46833f9546471f6b4f173873d8513
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2020-12-09 14:53:14 +0100

    adding variables

--- assets/Scripts/app.js
+++ assets/Scripts/app.js
@@ -2,11 +2,13 @@ const ATTACK_VALUE = 10;
 
 let chosenMaxLIfe = 100;
 let currentMonsterHealth = chosenMaxLIfe;
+let currentPLayerHealth = chosenMaxLIfe;
 
 adjustHealthBars(chosenMaxLIfe);
 
 function attackHandler() {
    const damage = dealMonsterDamage(ATTACK_VALUE);
+   currentMonsterHealth -= damage;
 }
 
 attackBtn.addEventListener('click',attackHandler);
