0079de3ae1aa1f24881f2be68b45a6cdde04b488
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2020-12-11 11:48:48 +0100

    simplified function

--- assets/Scripts/app.js
+++ assets/Scripts/app.js
@@ -8,8 +8,14 @@ let currentPlayerHealth = chosenMaxLIfe;
 
 adjustHealthBars(chosenMaxLIfe);
 
-function attackHandler() {
-   const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+   let maxDamage;
+   if (mode === 'ATTACK') {
+      maxDamage = ATTACK_VALUE;
+   } else if (mode === 'STRONG_ATTACK') {
+      maxDamage = STRONG_ATTACK_VALUE;
+   }
+   const damage = dealMonsterDamage(maxDamage);
    currentMonsterHealth -= damage;
    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
    currentPlayerHealth -= playerDamage;
@@ -21,18 +27,11 @@ function attackHandler() {
       alert('you have a draw!!');
    }
 }
+function attackHandler() {
+   attackMonster('ATTACK');
+}
 function strongAttackHandler() {
-   const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
-   currentMonsterHealth -= damage;
-   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-   currentPlayerHealth -= playerDamage;
-   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-      alert('you won! ');
-   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-      alert('you lost!');
-   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
-      alert('you have a draw!!');
-   }
+   attackMonster('STRONG_ATTACK');
 }
 
 attackBtn.addEventListener('click', attackHandler);
