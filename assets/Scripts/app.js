dbdc12516849fe49e8c70ac39bbade484d80c2cd
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2020-12-11 11:24:05 +0100

    added more function

--- assets/Scripts/app.js
+++ assets/Scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLIfe = 100;
@@ -17,8 +18,22 @@ function attackHandler() {
    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
       alert('you lost!');
    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
-      alert('you have a draw!!')
+      alert('you have a draw!!');
+   }
+}
+function strongAttackHandler() {
+   const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+   currentMonsterHealth -= damage;
+   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+   currentPlayerHealth -= playerDamage;
+   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+      alert('you won! ');
+   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+      alert('you lost!');
+   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+      alert('you have a draw!!');
    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
